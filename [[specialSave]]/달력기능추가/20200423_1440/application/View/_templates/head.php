fb87fda2610bd4748d1b0119f72e26c41dcf54bf
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-04-23 14:43:09 +0900

    Create head.php

---
+++ [[specialSave]]/달력기능추가/20200423_1440/application/View/_templates/head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+		<title>집진기</title>
+
+		<link rel="shortcut icon" type="image/png" href="/image/favicon.png">
+
+		<link rel="stylesheet" type="text/css" href="<?=URL?>css/reset.css">
+		<link rel="stylesheet" type="text/css" href="<?=URL?>css/ui.css">
+		<link rel="stylesheet" type="text/css" href="<?=URL?>css/style.css">
+		<link rel="stylesheet" type="text/css" href="<?=URL?>css/slick.css">
+		<link rel="stylesheet" type="text/css" href="<?=URL?>css/slick-theme.css">
+		<link rel="stylesheet" type="text/css" href="<?=URL?>css/jquery-ui.min.css">
+		<script src="<?=URL?>js/common/jquery-3.1.1.min.js"></script>
+		<script src="<?=URL?>js/common/jquery-ui-1.12.1.js"></script>
+		<script src="<?=URL?>js/common/slick.js"></script>
+		<script src="<?=URL?>js/common/ui.js"></script>
+		<script src="<?=URL?>js/common/common.js"></script>
+	</head>
+	<body>
