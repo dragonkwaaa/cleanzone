5972bdcd61a2d2a1cf9b00f830d29153d04f2026
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-04-23 14:45:01 +0900

    Create statusWeb.php

---
+++ [[specialSave]]/달력기능추가/20200423_1440/application/View/main/statusWeb.php
@@ -0,0 +1,180 @@
+
+<div class="container webSort">
+	<header>
+		<h1 class="logo">
+			<a href="javascript:void(0)" class="titleBtn">
+				CleanZone
+			</a>
+		</h1>
+	</header>
+	<div class="content">
+		<div class="tableGroup">
+
+
+
+			<div class="tableMenu">
+				<div class="sbox">
+					<select>
+						<option value="">- 지점 선택 -</option>
+						<option value="">광명 분체도장 공장</option>
+						<option value="">오산 자동차 외장 작업장</option>
+					</select>
+				</div>
+				<div class="sbox">
+					<select>
+						<option value="">- 구역 선택 -</option>
+						<option value="">도장 작업장</option>
+						<option value="">창고</option>
+					</select>
+				</div>
+			</div>
+
+
+			<!-- :: ksg_20200423_1210 open : 검색 부분의 테이블화 파트 -->
+			<table class="tableMenu">
+				<colgroup>
+					<col width="100">
+					<col width="500">
+				</colgroup>
+				<tbody>
+				<tr>
+					<th>지점 선택</th>
+					<td>
+						<div class="sbox">
+							<select>
+								<option value="">- 지점 선택 -</option>
+								<option value="">광명 분체도장 공장</option>
+								<option value="">오산 자동차 외장 작업장</option>
+							</select>
+						</div>
+						<div class="sbox">
+							<select>
+								<option value="">- 구역 선택 -</option>
+								<option value="">도장 작업장</option>
+								<option value="">창고</option>
+							</select>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th>기간선택</th>
+					<td>
+						<input class="tbox dateBox" id="startDate" readonly/>
+						<span>~</span>
+						<input class="tbox dateBox"  id="endDate" readonly/>
+						<div class="btnGroup ml15">
+							<a	href="javascript:void(0);">오늘</a>
+							<a	href="javascript:void(0);">어제</a>
+							<a	href="javascript:void(0);">일주일</a>
+							<a	href="javascript:void(0);">1개월</a>
+							<a	href="javascript:void(0);">3개월</a>
+							<a	href="javascript:void(0);">전체</a>
+						</div>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+			<!-- :: ksg_20200423_1210 close : 검색 부분의 테이블화 파트 -->
+
+
+
+			<div class="tableBox">
+				<table class="listTable">
+					<colgroup>
+					<col width="80">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="100">
+					<col width="200">
+					</colgroup>
+					<thead>
+					<tr>
+						<th>번호</th>
+						<th>온도1(PT100)</th>
+						<th>온도2(PT100)</th>
+						<th>전류1(CT)</th>
+						<th>전류2(CT)</th>
+						<th>센서1(전류방식)</th>
+						<th>센서2(전류방식)</th>
+						<th>센서3(전류방식)</th>
+						<th>센서4(전류방식)</th>
+						<th>릴레이출력1</th>
+						<th>릴레이출력2</th>
+						<th>TTL출력1</th>
+						<th>TTL출력2</th>
+						<th>TTL출력3</th>
+						<th>TTL출력4</th>
+						<th>o.c.출력1</th>
+						<th>o.c.출력2</th>
+						<th>o.c.출력3</th>
+						<th>o.c.출력4</th>
+						<th>마지막 업데이트</th>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td>081415</td>
+						<td>28.5</td>
+						<td>0.75</td>
+						<td>0.75</td>
+						<td>0.75</td>
+						<td>교류</td>
+						<td>교류</td>
+						<td>교류</td>
+						<td>교류</td>
+						<td>1.5</td>
+						<td>1.5</td>
+						<td>2</td>
+						<td>2</td>
+						<td>2</td>
+						<td>2</td>
+						<td>5.0</td>
+						<td>5.0</td>
+						<td>5.0</td>
+						<td>5.0</td>
+						<td>2020-04-01 12:00</td>
+					</tr>
+					<tr>
+						<td>081416</td>
+						<td>28.5</td>
+						<td>0.75</td>
+						<td>0.75</td>
+						<td>0.75</td>
+						<td>교류</td>
+						<td>교류</td>
+						<td>교류</td>
+						<td>교류</td>
+						<td>1.5</td>
+						<td>1.5</td>
+						<td>2</td>
+						<td>2</td>
+						<td>2</td>
+						<td>2</td>
+						<td>5.0</td>
+						<td>5.0</td>
+						<td>5.0</td>
+						<td>5.0</td>
+						<td>2020-04-01 12:00</td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="/js/common/datePick.js"></script>
